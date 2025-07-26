d3cf935a571f64da26118e326263126dda083819
Gianluigi De Rose <blugiander@gmail.com>
2025-07-26 08:01:17 +0200

    push hetzner

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+SERVER_HOST = "188.245.238.160" # O "0.0.0.0" sul server stesso se è in ascolto su tutte le interfacce
+SERVER_PORT = 8765 # Questa sarà l'unica porta usata per entrambi
+CLIENT_ID = "cliente-001"
+# Rimuovi SERVER_PORT_TECHNICIAN se esisteva
